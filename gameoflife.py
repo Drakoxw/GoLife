2324302b64bb85ef9ba0af2935a6744c7965c557
Drakoxw <dragon12xw@gmail.com>
2020-05-16 01:19:40 -0500

    juego_pausable

--- gameoflife.py
+++ gameoflife.py
@@ -14,8 +14,8 @@ bg = 25,25,25
 screen.fill(bg)
 
 
-nxC = 50# número de celdas
-nyC = 50
+nxC = 45# número de celdas
+nyC = 45
 dimCW = width / nxC#se le da la dimensión
 dimCh = height / nyC
 
@@ -28,6 +28,7 @@ gameState = np.zeros((nxC, nyC))
 gameState[5,3] = 1
 gameState[5,4] = 1
 gameState[5,5] = 1
+gameState[5,6] = 1
 #automate movil
 gameState[21,21] = 1
 gameState[22,22] = 1
@@ -35,43 +36,54 @@ gameState[22,23] = 1
 gameState[21,23] = 1
 gameState[20,23] = 1
 
-
+#control de pausa del juego
+pauseExect = False
 
 while True:#recorer los dos ejes
 
     NewgameState = np.copy(gameState)
 
     #limpiar pantalla en cada iteración
-    screen.fill(bg)
+    screen.fill(bg)#pintandola como al principio
 
     #diley
     time.sleep(0.1)
 
+    #registra eventos de raton y teclado
+    ev = pygame.event.get()
+
+    for event in ev:
+        if event.type == pygame.KEYDOWN:
+            pauseExect = not pauseExect
+
+
     for y in range(0, nxC):
         for x in range(0, nyC):
 
-            #calcular el numero de vecinos cercanos
-            n_neigh = gameState[(x - 1)% nxC, (y-1) % nyC]+ \
-                      gameState[(x)    % nxC, (y-1) % nyC]+ \
-                      gameState[(x + 1)% nxC, (y-1) % nyC]+ \
-                      gameState[(x - 1)% nxC, (y) % nyC]+ \
-                      gameState[(x + 1)% nxC, (y) % nyC]+ \
-                      gameState[(x - 1)% nxC, (y+1) % nyC]+ \
-                      gameState[(x)    % nxC, (y+1) % nyC]+ \
-                      gameState[(x + 1)% nxC, (y+1) % nyC]
+            if not pauseExect:
 
-            # Regla 1: una celular con 3 celulas viva,revive
-            if gameState[y, x] == 0 and n_neigh == 3:
-                NewgameState[x, y] = 1
+                #calcular el numero de vecinos cercanos
+                n_neigh = gameState[(x - 1)% nxC, (y-1) % nyC]+ \
+                        gameState[(x)    % nxC, (y-1) % nyC]+ \
+                        gameState[(x + 1)% nxC, (y-1) % nyC]+ \
+                        gameState[(x - 1)% nxC, (y) % nyC]+ \
+                        gameState[(x + 1)% nxC, (y) % nyC]+ \
+                        gameState[(x - 1)% nxC, (y+1) % nyC]+ \
+                        gameState[(x)    % nxC, (y+1) % nyC]+ \
+                        gameState[(x + 1)% nxC, (y+1) % nyC]
 
-            #Regla2: una celula viva con menos d 2, o mas de 3 muere
-            elif gameState[x, y] == 1 and (n_neigh < 2 or n_neigh > 3):
-                NewgameState[x, y] = 0
+                # Regla 1: una celular con 3 celulas viva,revive
+                if gameState[y, x] == 0 and n_neigh == 3:
+                    NewgameState[x, y] = 1
+
+                #Regla2: una celula viva con menos d 2, o mas de 3 muere
+                elif gameState[x, y] == 1 and (n_neigh < 2 or n_neigh > 3):
+                    NewgameState[x, y] = 0
 
             poly =[((x) * dimCW, y * dimCh),
-            ((x+1) * dimCW, y * dimCh),
-            ((x+1) * dimCW, (y+1) * dimCh),
-            ((x) * dimCW, (y+1) * dimCh)]
+                ((x+1) * dimCW, y * dimCh),
+                ((x+1) * dimCW, (y+1) * dimCh),
+                ((x) * dimCW, (y+1) * dimCh)]
 
             if NewgameState[x, y] == 0:
                 pygame.draw.polygon(screen, (128,128,128), poly,1)
